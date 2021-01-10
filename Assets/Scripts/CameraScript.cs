574deb60d505321f347a6beac98c7a5966ba5f45
Nikhil Gupta <nikgupta06@gmail.com>
2021-01-10 15:51:02 -0600

    Rewrite code and lerp behavior for camera following the player.

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -1,55 +1,60 @@
 using UnityEngine;
-using System.Collections;
 
 public class CameraScript : MonoBehaviour
 {
-    // Smooth camera taken from here
+    // Smooth camera taken and modified heavily from here
     // https://gamedev.stackexchange.com/questions/114742/how-can-i-make-camera-to-follow-smoothly
 
     // The target to follow
     public Transform target;
 
-    // How far away in the x-z plane should the camera be from the target
-    public float distance;
-
-    // How high should the camera be above the target
-    public float height;
+    // Calculate height and distance from player with hypotenuse and angle
+    public float hypotenuse;
+    public float angleOfDepression;
+    public bool inDegrees;
 
     // How quickly should changes in height or direction be applied
-    public float heightDamping;
+    public float yDamping;
+    public float zDamping;
+    public float xDamping;
     // public float rotationDamping;
 
-    // float wantedRotationAngle;
-    float wantedHeight;
+    // Where the camera should be positioned
+    private Vector3 _positionRelativeToTarget;
 
-    // float currentRotationAngle;
-    float currentHeight;
+    public void Start()
+    {
+        // Ensure angle is in radians
+        float angleOfDepressionRadians = inDegrees ? angleOfDepression * Mathf.Deg2Rad : angleOfDepression;
+        // Calculate distances away from player
+        float horizontalDistance = Mathf.Cos(angleOfDepressionRadians) * hypotenuse;
+        float verticalDistance = Mathf.Sin(angleOfDepressionRadians) * hypotenuse;
+        _positionRelativeToTarget = Vector3.back * horizontalDistance + Vector3.up * verticalDistance;
 
-    Quaternion currentRotation;
+        // Point at target - rotate around x axis
+        transform.rotation = Quaternion.AngleAxis(angleOfDepressionRadians*Mathf.Rad2Deg, Vector3.right);
+    }
 
     void LateUpdate()
     {
         if (target)
         {
+            Vector3 currentPosition = transform.position;
+            Vector3 wantedPosition = target.transform.position + _positionRelativeToTarget;
+            float newX = Mathf.Lerp(currentPosition.x, wantedPosition.x, xDamping * Time.deltaTime);
+            float newY = Mathf.Lerp(currentPosition.y, wantedPosition.y, yDamping * Time.deltaTime);
+            float newZ = Mathf.Lerp(currentPosition.z, wantedPosition.z, zDamping * Time.deltaTime);
+            
             // Calculate the current rotation angles
             // wantedRotationAngle = target.eulerAngles.y;
-            wantedHeight = target.position.y + height;
-            // currentRotationAngle = transform.eulerAngles.y;
-            currentHeight = transform.position.y;
+            // // currentRotationAngle = transform.eulerAngles.y;
             // Damp the rotation around the y-axis
             // currentRotationAngle = Mathf.LerpAngle(currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
-            // Damp the height
-            currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * Time.deltaTime);
             // Convert the angle into a rotation
             // currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
 
-            // Set the position of the camera to distance units in the -z direction relative to the player
-            transform.position = target.position + Vector3.back * distance;
-            // Set the height of the camera to be currentHeight units above the player
-            transform.position = new Vector3(transform.position.x, currentHeight, transform.position.z);
-
-            // Always look at the target
-            transform.LookAt(target);
+            // Set new position of camera
+            transform.position = new Vector3(newX, newY, newZ);
         }
     }
 }
