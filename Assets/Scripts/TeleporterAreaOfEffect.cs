a8068dbc19266309ef073e08520d6630953466c9
Nikhil Gupta <nikgupta06@gmail.com>
2021-01-20 23:41:37 -0600

    Clean up teleporter and adjust camera pan settings.

--- Assets/Scripts/TeleporterAreaOfEffect.cs
+++ Assets/Scripts/TeleporterAreaOfEffect.cs
@@ -3,42 +3,26 @@
 public class TeleporterAreaOfEffect : MonoBehaviour
 {
     private float _time;
-    private const float TimeThreshold = 5f;
+    private const float TimeThreshold = 3f;
 
     private void OnTriggerEnter(Collider other)
     {
         PlayerScript player = other.GetComponent<PlayerScript>();
 
-        Debug.Log("Player has entered the teleportation radius!");
-        _time = Time.unscaledTime;
+        if (player != null)
+            _time = Time.unscaledTime;
     }
 
     private void OnTriggerStay(Collider other)
     {
         PlayerScript player = other.GetComponent<PlayerScript>();
 
-        if (_time + TimeThreshold > Time.unscaledTime)
+        if (player != null)
         {
-            Debug.Log("Player is waiting for teleportation...");
-        }
-        else
-        {
-            Debug.Log("Player should have teleported!");
-            player.TeleportTo(new Vector3(0f, 0f, 150f));
+            if (_time + TimeThreshold <= Time.unscaledTime)
+            {
+                player.TeleportTo(new Vector3(0f, 0f, 150f));
+            }
         }
     }
-
-    // private void OnTriggerExit(Collider other)
-    // {
-    //     PlayerScript player = other.GetComponent<PlayerScript>();
-    //
-    //     if (_time + TimeThreshold > Time.unscaledTime)
-    //     {
-    //         Debug.Log("Player is leaving - should not have teleported!");
-    //     }
-    //     else
-    //     {
-    //         Debug.Log("Player is leaving - should have teleported!");
-    //     }
-    // }
 }
